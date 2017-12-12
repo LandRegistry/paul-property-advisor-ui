997054dbad3d04195c46fe37182dc23c421975ed
Simon Chapman <sdchapman@gmail.com>
2017-12-12 10:41:14 +0000

    Add clickable instant mortgage link

--- app/assets/src/components/Message.jsx
+++ app/assets/src/components/Message.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
 
-const Message = ({message}) => (
-    <li className={message.bot ? "chat__item chat__item--bot" : "chat__item"}>{message.message}</li>
+const Message = ({ message }) => (
+    // Output the message from the bot, replacing any INSTANTMORTGAGELINK text with a clickable link
+    <li className={message.bot ? "chat__item chat__item--bot" : "chat__item"}>{message.message.replace('INSTANTMORTGAGELINK','')} {(message.message.includes('INSTANTMORTGAGELINK')) ? <a href='https://hmlr-ds-instantmortgageui.eu-gb.mybluemix.net'> here </a> : ''} </li>
 );
 
 export default Message;
